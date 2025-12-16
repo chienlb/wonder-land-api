42ce89cd0a6d22136a17be1f81cf8b5924704d00
La Bao Chien <4801104018@student.hcmue.edu.vn>
2025-12-16 23:23:41 +0700

    feat: update code history code

--- src/app/modules/history-invitations/history-invitations.controller.ts
+++ src/app/modules/history-invitations/history-invitations.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Post, Body, Logger } from '@nestjs/common';
+import { Controller, Post, Body, Logger, Param, Delete, Get, Put, Query } from '@nestjs/common';
 import { HistoryInvitationsService } from './history-invitations.service';
 import { CreateHistoryInvitationDto } from './dto/create-history-invitation.dto';
 import mongoose from 'mongoose';
 import { ok } from 'src/app/common/response/api-response';
+import { ApiTags, ApiOperation, ApiBody, ApiResponse, ApiParam, ApiQuery, ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { UseGuards } from '@nestjs/common';
+import { PaginationDto } from '../pagination/pagination.dto';
+import { UpdateHistoryInvitationDto } from './dto/update-history-invitation.dto';
 
 @Controller('history-invitations')
+@ApiTags('History Invitations')
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'))
 export class HistoryInvitationsController {
   private readonly logger = new Logger(HistoryInvitationsController.name);
 
@@ -13,6 +21,28 @@ export class HistoryInvitationsController {
   ) { }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new history invitation' })
+  @ApiBody({
+    type: CreateHistoryInvitationDto,
+    description: 'The history invitation to create',
+    examples: {
+      example1: {
+        value: {
+          userId: '1234567890',
+          code: 'INVITE',
+          invitedAt: new Date(),
+          status: 'accepted',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      },
+    },
+  })
+  @ApiResponse({ status: 200, description: 'History invitation created successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
   async create(@Body() createHistoryInvitationDto: CreateHistoryInvitationDto) {
     const session = await mongoose.startSession();
     session.startTransaction();
@@ -26,5 +56,53 @@ export class HistoryInvitationsController {
     await session.commitTransaction();
     await session.endSession();
     return ok(result, 'History invitation created successfully', 200);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all history invitations' })
+  @ApiResponse({ status: 200, description: 'History invitations fetched successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  async findAll(@Query() paginationDto: PaginationDto) {
+    const result = await this.historyInvitationsService.findAllHistoryInvitations(paginationDto);
+    return ok(result, 'History invitations fetched successfully', 200);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get a history invitation by id' })
+  @ApiResponse({ status: 200, description: 'History invitation fetched successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  async findOne(@Param('id') id: string) {
+    const result = await this.historyInvitationsService.findOneHistoryInvitation(id);
+    return ok(result, 'History invitation fetched successfully', 200);
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'Update a history invitation by id' })
+  @ApiResponse({ status: 200, description: 'History invitation updated successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  async update(@Param('id') id: string, @Body() updateHistoryInvitationDto: UpdateHistoryInvitationDto) {
+    const result = await this.historyInvitationsService.updateHistoryInvitation(id, updateHistoryInvitationDto);
+    return ok(result, 'History invitation updated successfully', 200);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete a history invitation by id' })
+  @ApiResponse({ status: 200, description: 'History invitation deleted successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  async delete(@Param('id') id: string) {
+    const result = await this.historyInvitationsService.remove(id);
+    return ok(result, 'History invitation deleted successfully', 200);
   }
 }
--- src/app/modules/history-invitations/history-invitations.service.ts
+++ src/app/modules/history-invitations/history-invitations.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateHistoryInvitationDto } from './dto/create-history-invitation.dto';
 import { UpdateHistoryInvitationDto } from './dto/update-history-invitation.dto';
 import {
@@ -8,6 +8,7 @@ import {
 import { ClientSession, Model } from 'mongoose';
 import { UsersService } from '../users/users.service';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from '../pagination/pagination.dto';
 
 @Injectable()
 export class HistoryInvitationsService {
@@ -38,12 +39,33 @@ export class HistoryInvitationsService {
   // ============================================================
   // FIND ALL
   // ============================================================
-  async findAllHistoryInvitations(session?: ClientSession) {
-    return await this.historyModel
-      .find({}, null, session ? { session } : {})
-      .populate('userId')
-      .lean()
-      .exec();
+  async findAllHistoryInvitations(paginationDto: PaginationDto, session?: ClientSession) {
+    try {
+      const { page, limit, sort, order } = paginationDto;
+      const skip = (page - 1) * limit;
+      const historyInvitations = await this.historyModel
+        .find({}, null, session ? { session } : {})
+        .skip(skip)
+        .limit(limit)
+        .sort({ [sort]: order === 'asc' ? 1 : -1 })
+        .populate('userId')
+        .lean()
+        .exec();
+      const total = await this.historyModel.countDocuments();
+      const totalPages = Math.ceil(total / limit);
+      const nextPage = page < totalPages ? page + 1 : null;
+      const prevPage = page > 1 ? page - 1 : null;
+      return {
+        data: historyInvitations,
+        total,
+        totalPages,
+        nextPage,
+        prevPage,
+      };
+    }
+    catch (error) {
+      throw new BadRequestException('Failed to find all history invitations. Please try again later.');
+    }
   }
 
   // ============================================================
